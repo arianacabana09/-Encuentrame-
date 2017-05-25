95212c4a9af5b7b9c848c09bbdb892c6c0dac3c0
Ariana Cabana <acabana.0904@gmail.com>
2017-05-25 18:33:21 -0500

    funcion buscame

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,40 @@
 function initMap(){
-  alert();
-    var laboratoriaLima = {lat: -12.119127, lng: -77.0349046};
-    var map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 18,
-      center: laboratoriaLima
-    });
 
-    var markadorLaboratoria = new google.maps.Marked({
-      position: laboratoriaLima,
-      map: map
-    })
+  var laboratoriaLima = {lat: -12.119127, lng: -77.0349046};
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 18,
+    center: laboratoriaLima
+  });
+
+  var markadorLaboratoria = new google.maps.Marker({
+    position: laboratoriaLima,
+    map: map
+  });
+
+  function buscar(){
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(functionExito, functionError);
+    }
+  }
+
+  //
+  var latitud, longitud;
+  var functionExito = function(position){
+    latitud=position.coords.latitude;
+    longitud=position.coords.longitude;
+
+    map.setZoom(18);
+    map.setCenter({lat:latitud, lng:longitud});
+
+    var miUbicacion =  new google.maps.Marker({
+      position:{lat: latitud, lng: longitud}, map: map
+    });
+  }
+
+  //
+  var functionError = function(error){
+    alert("Tenemos un problema con encontrar tu ubicacion");
+  }
+
+  document.getElementById("encuentrame").addEventListener("click",buscar);
 }
