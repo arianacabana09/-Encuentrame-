c8cebe3440af502b9716a3d3dbe5679a97bc09c3
Ariana Cabana <acabana.0904@gmail.com>
2017-05-26 11:10:56 -0500

    agregando Bootstrap y formulario al mapa

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
+// Google Maps
 function initMap(){
-
   var laboratoriaLima = {lat: -12.119127, lng: -77.0349046};
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 18,
@@ -17,7 +17,6 @@ function initMap(){
     }
   }
 
-  //
   var latitud, longitud;
   var functionExito = function(position){
     latitud=position.coords.latitude;
@@ -31,10 +30,17 @@ function initMap(){
     });
   }
 
-  //
   var functionError = function(error){
     alert("Tenemos un problema con encontrar tu ubicacion");
   }
 
   document.getElementById("encuentrame").addEventListener("click",buscar);
 }
+
+//input 4 4 2 2
+
+var inputPartida = document.getElementById("punto-partida"),
+    inputPartida = document.getElementById("punto-destino");
+
+new google.maps.places.Autocomplete(inputPartida);
+new google.maps.places.Autocomplete(inputDestino);
